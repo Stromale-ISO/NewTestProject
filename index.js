fc651eea05f26567232178bf25390036af8154d0
Stromale-ISO <juhimenkoroman@gmail.com>
2024-07-29 11:04:22 +0500

    Update index.js
    авы

--- index.js
+++ index.js
@@ -2,12 +2,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World ver3!');
+  res.send('Hello World ver2!');
 });
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-exports.app = app;
